6b1c2badcd30a511dd63c7d51bebc9347d53cb40
LogicalShot <ge9305@gmail.com>
2018-02-13 17:49:11 +0900

    HTML5 Study Test last!!

---
+++ 로그인 프로그램/open.js
@@ -0,0 +1,13 @@
+id=prompt("ID");
+if(id == "std01") (id == "std02") (id == std03") {
+	password=prompt("Password");
+	if(password === "1111") (password === "2222") (password === "3333") {
+		location.href="login.html"
+	}
+	else {
+		location.href="error.html"
+	}
+}
+else {
+	location.href="errorS.html"
+}
